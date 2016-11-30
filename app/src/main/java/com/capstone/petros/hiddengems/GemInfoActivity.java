31da7088d311d442dacc44af750857f42433a5f7
Peter Koutras <pkoutras@terpmail.umd.edu>
2016-11-30 18:28:34 -0500

    Added Gem Info
    Added the Gem Info Activity and UI. Just the UI, however - no
    interactions are hooked up.

---
+++ app/src/main/java/com/capstone/petros/hiddengems/GemInfoActivity.java
@@ -0,0 +1,13 @@
+package com.capstone.petros.hiddengems;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GemInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gem_info);
+    }
+}
