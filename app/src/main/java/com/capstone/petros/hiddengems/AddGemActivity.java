91971eef4b881a40c15f48a7a1d8ac5f4796171c
yfujimot <yosh.fuj@gmail.com>
2016-11-30 21:45:50 -0500

    add gem transitions

--- app/src/main/java/com/capstone/petros/hiddengems/AddGemActivity.java
+++ app/src/main/java/com/capstone/petros/hiddengems/AddGemActivity.java
@@ -1,7 +1,12 @@
 package com.capstone.petros.hiddengems;
 
+import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class AddGemActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class AddGemActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_gem);
+
+        final ImageButton backButton = (ImageButton)findViewById(R.id.addGemBackButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        final Button submitButton = (Button)findViewById(R.id.submitGemButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Validate form and save new object, pass back as parcelable
+
+                Intent data = new Intent();
+
+                setResult(RESULT_OK, data);
+                finish();
+            }
+        });
     }
 }
