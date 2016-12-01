8bcb43658d698e416ca6d533374e9f69e1f9f54b
yfujimot <yosh.fuj@gmail.com>
2016-11-30 20:12:13 -0500

    init add gem page

---
+++ app/src/main/java/com/capstone/petros/hiddengems/AddGemActivity.java
@@ -0,0 +1,13 @@
+package com.capstone.petros.hiddengems;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddGemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_gem);
+    }
+}
